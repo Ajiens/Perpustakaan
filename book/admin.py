844d1122f841f229f77d18cbb17038a300ac3ec3
alwantakaditama@gmail.com <alwantakaditama@gmail.com>
2023-12-09 23:00:13 +0700

    ready to deployment

--- book/admin.py
+++ book/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 from .models import Book
-from deskripsi_buku.models import Review
 
-myModels = [Book, Review]  # iterable list
+from deskripsi_buku.models import Review
+from add_wishlist.models import WishlistItem
+from pinjam_buku.models import Borrow
+
+myModels = [Book, Review, WishlistItem, Borrow]  # iterable list
 admin.site.register(myModels)
 # Register your models here.
