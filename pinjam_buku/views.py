ef72818e9ec6a9028cb844aed576db51907d4f35
mahirairgn <mahirairgn@gmail.com>
2023-12-17 00:01:42 +0700

    benerin pinjam_buku

--- pinjam_buku/views.py
+++ pinjam_buku/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, JsonResponse
@@ -17,11 +18,17 @@ def show_borrow(request):
     all_borrowed_books = [item.book for item in borrowed_books]
     return render(request, 'peminjaman.html', {'borrowed_books': all_borrowed_books})
 
-def get_books_json(request):
-    user_id = request.COOKIES.get('user') #Ambil Cookie id
-    role_user = User.objects.get(username=user_id)
 
-    borrow_items = Borrow.objects.filter(user=role_user)
+def get_books_json(request):
+    print()
+    if request.user.is_authenticated:
+        user = request.user
+        print("kesini")
+    else:
+        user_id = request.COOKIES.get('user') #Ambil Cookie id
+        user = User.objects.get(username=user_id)
+
+    borrow_items = Borrow.objects.filter(user=user)
     borrowed_books = [item.book for item in borrow_items]
     context = [
         {
@@ -94,3 +101,33 @@ def pinjam_buku_ajax(request,id):
         else :
             return HttpResponseNotFound()
     return HttpResponseNotFound()
+
+
+@csrf_exempt
+def pinjam_buku_flutter(request,id):
+    print("halo")
+  
+    if request.method == 'POST':
+        print ("apakabar")
+        data = json.loads(request.body)
+        print("baik")
+        user = request.COOKIES.get('user') 
+        print(user)
+        if (user == None):
+            user = request.user
+        print(user)
+        print("ininih")
+        lama_peminjaman = data['lama_peminjaman']
+        book = get_object_or_404(Book, pk=id)
+        if book.is_available:
+            book_borrow_exists = Borrow.objects.filter(user=user, book=book).exists()
+            if not book_borrow_exists:
+                book_borrow = Borrow(user=user, book=book,lama_peminjaman = lama_peminjaman, is_dikembalikan=False)
+                book_borrow.save()
+                book.is_available = False
+                book.save()
+            return JsonResponse({"status": "success"}, status=200)
+        else:
+         return JsonResponse({"status": "error"}, status=401) 
+    else:
+        return JsonResponse({"status": "error"}, status=401)
