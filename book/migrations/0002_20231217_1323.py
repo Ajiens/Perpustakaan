b095df680361cb80837612035765713bce00389b
prasetyoadii <prasetyo.adi21@ui.ac.id>
2023-12-17 02:47:56 +0700

    mengerjakan fungsionalitas modul flutter

---
+++ book/migrations/0002_20231217_1323.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+from django.core.management import call_command
+
+
+def load_my_initial_data(apps, schema_editor):
+    call_command("loaddata", "books.json")
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('book', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_my_initial_data),
+    ]
